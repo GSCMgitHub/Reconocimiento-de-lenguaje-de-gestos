09ee0f01e9895d96197fea0bb646e57c68991385
GSCMgitHub <sebascoellom@gmail.com>
2024-12-31 20:32:19 -0500

    Arreglo a json_dump.py
    json_dump.py ahora si llena words_list.json como debe de ser

--- json_dump.py
+++ json_dump.py
@@ -9,17 +9,18 @@ def j_dump(file_path_LIST, file_path_DICT, info_path, debug=False):
     keys = os.listdir(info_path)
     values = []
 
-    words_list = { 
-        "word_ids": []
-    }
-
     for word in keys:
         fixed_text = word.replace('_', ' ')
         if debug:
             print('Texto arreglado: ', fixed_text)
         values.append(fixed_text)     
-        
     dictionary = dict(zip(keys, values))
+
+    words_list = { 
+        "word_ids": [
+
+        ]
+    }
     words_list["word_ids"].extend(values)   
 
     # 2. Lee el archivo JSON (diccionario)
@@ -27,7 +28,7 @@ def j_dump(file_path_LIST, file_path_DICT, info_path, debug=False):
         data_dict = json.load(file_d)
 
     with open(file_path_LIST, 'r') as file_l:
-        data_lista = json.load(file_l)
+        data_list = json.load(file_l)
     
     #DEBUG 1
     if debug:
@@ -40,12 +41,14 @@ def j_dump(file_path_LIST, file_path_DICT, info_path, debug=False):
 
         print('Archivo (diccionario): ', file_d)
         print('Archivo (lista): ', file_l)
-        print('Contenido JSON anterior (lista): ', data_lista)
+        print('Contenido JSON anterior (lista): ', data_list)
         print('Contenido JSON anterior (diccionario): ', data_dict)
         print('\n')
 
     # 3. Lo escribe de vuelta a los archivos JSON
-    json_list = json.dumps(words_list, indent=4) #LISTA
+
+    with open(file_path_LIST, 'w') as file_l: #LISTA
+        file_l = json.dump(words_list, file_l, indent=4) 
 
     with open(file_path_DICT, 'w') as file_d: #DICCIONARIO
         file_d = json.dump(dictionary, file_d, indent=4)
@@ -55,7 +58,6 @@ def j_dump(file_path_LIST, file_path_DICT, info_path, debug=False):
         with open(file_path_DICT, 'r') as file_d:
             new_data_dict = json.load(file_d)
             print('Contenido JSON actual (diccionario): ', new_data_dict)
-
         with open(file_path_LIST, 'r') as file_d:
             new_data_list = json.load(file_d)
             print('Contenido JSON actual (lista): ', new_data_list)
