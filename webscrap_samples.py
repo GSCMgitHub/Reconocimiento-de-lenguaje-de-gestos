ef8696f76945abb5dff6e91b38504871263c0014
GSCMgitHub <sebascoellom@gmail.com>
2025-02-05 21:41:51 -0500

    Mejora a webscrap_samples.py
    El código ha sido actualizado y ahora es una función que solo se ejecuta si se ejecuta este script en particular. Se han añadido comentarios y se han cambiado nombres de variables para mayor claridad. Pasos en read_me actualizados.

--- webscrap_samples.py
+++ webscrap_samples.py
@@ -3,125 +3,129 @@ import requests
 from constants import VIDEO_FILE_INPUT_PATH
 from bs4 import BeautifulSoup
 
-# URL de la página web a scrapear
-url_main = 'https://www.utm.edu.ec/inclusion/index.php/servicios/apoyo-educativo/glosario-de-lengua-de-senias-ecuatorianas-lsec'
+'''
+    ### OBTENER MUESTRAS VÍA SCRAPPING
+    Busca y descarga los videos, y los guarda en la carpeta de entrada (VIDEO_FILE_INPUT_PATH).
+    Cabe decir que esto funciona para un sitio en específico (me refiero al enlace en url_specific).
+'''
 
-# Lista de elementos con length 1 (declaración) y valores href (sirven para encontrar los enlaces donde están los videos)
-letras = []
-href_s = []
-
-#solicitud GET
-response = requests.get(url_main, verify=False)
-
-# Verifica que la solicitud fue exitosa
-if response.status_code == 200:
-    # Creación de un objeto BeautifulSoup con el contenido HTML de la página
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Se encuentra y se extra todo lo que tenga el tag "a"
-    elementos = soup.find_all('a')
-
-# Busca elementos que sean de solo una letra y sus valores href
-    #print("Letras:")
-    for elemento in elementos:
-        if len(elemento.text) == 1:
-            letras.append(elemento.text)
-            href = elemento.get('href')
-            href_s.append(href)
-            #print(elemento.text)
-
-    #print("Letras y valores enlaces listos.")
-    diccionario_href = dict(zip(letras,href_s))
-    #print("\nDiccionario listo (letras y href's): ")
-
-    #for letra, href in diccionario_href.items():
-    #    print(f"Letra: {letra}")
-    #    print(f"Valor href: {href}")
-    #    print()
-
+def webscrap_samples(url_specific = 'https://www.utm.edu.ec/inclusion/index.php/servicios/apoyo-educativo/glosario-de-lengua-de-senias-ecuatorianas-lsec', url_general = 'https://www.utm.edu.ec'):
     
-#URL 2: hecha para encajar con el valor href
-    url_2 = 'https://www.utm.edu.ec'
-#src_s será los links de google drive sin modificar
-    src_s = []
+    '''
+        ### 1.  Con url_specific, se buscarán valores href, los cuales serán usados para encontrar 
+                las urls donde se encuentran los videos.
 
-    #print("Enlaces de videos (Sin modificar):\n")
-    for letra, href in diccionario_href.items():
+                Aquí un ejemplo de un valor href: 
+                /inclusion/index.php/servicios/apoyo-educativo/glosario-de-lengua-de-senias-ecuatorianas-lsec/category/3-a
+    '''
+    # Lista de elementos con length 1 (declaración) y valores href (sirven para encontrar los enlaces donde están los videos)
+    letras = []
+    href_s = []
 
-        sub_url = url_2 + href
-        sub_response = requests.get(sub_url, verify=False)
+    #solicitud GET
+    response = requests.get(url_specific, verify=False)
 
-        if sub_response.status_code == 200:
-            sub_soup = BeautifulSoup(sub_response.content, 'html.parser')
-            iframes = sub_soup.find_all('iframe', src=True)
-            print()
+    # Verifica que la solicitud fue exitosa
+    if response.status_code == 200:
+        # Creación de un objeto BeautifulSoup con el contenido HTML de la página
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-            if len(iframes) == 0: #Avisar si no encuentra ningún iframe.
-                print('No iframes encontrados para la letra:',letra,'\n')
-            else:
-                #print(letra)
-                #print()
-                for iframe in iframes:
-                    src = iframe.get('src')
-                    src_s.append(src)
-                    #print(src)
-                #print()
-        else:
-            print('Error al realizar la solicitud:', sub_response.status_code)
-            break
-            
-    
-    video_links = []
+        # Se encuentra y se extra todo lo que tenga el tag "a"
+        elementos = soup.find_all('a')
 
-    print('Links de descarga:')
-    for src in src_s:
-        url_parte1 = src.replace('https://drive.google.com/file/d/','https://drive.usercontent.google.com/uc?id=')
-        url_parte2 = url_parte1.replace('/preview', '&export=download')
-        print(url_parte2,'\n')
-        video_links.append(url_parte2)
+    # Busca elementos que sean de un caracter (letra)  y sus valores href
+        for elemento in elementos:
+            if len(elemento.text) == 1:
+                letras.append(elemento.text)
+                href = elemento.get('href')
+                href_s.append(href)
 
+        diccionario_href = dict(zip(letras,href_s))
 
-    video_amount = 0
+        '''
+            2.  
+                2.1. Con url_general, se formarán las url's donde están los videos.
 
-    for link in video_links:
-        download_response = requests.get(link, verify=False)
-        
-        if download_response.status_code == 200:
-            download_soup = BeautifulSoup(download_response.content, 'html.parser')
-            content_disposition = download_response.headers.get('content-disposition')
-            
-            #Encuentra el nombre del archivo en los encabezados
-            if content_disposition:
-                filename = content_disposition.split('filename=')[1].strip('"')
-                SAVE_VID_PATH = os.path.join(VIDEO_FILE_INPUT_PATH, filename)
+                    Valor href: 
+                        /inclusion/index.php/servicios/apoyo-educativo/glosario-de-lengua-de-senias-ecuatorianas-lsec/category/3-a
+                    Entonces, url_general + valor href =
+                        https://www.utm.edu.ec/inclusion/index.php/servicios/apoyo-educativo/glosario-de-lengua-de-senias-ecuatorianas-lsec/category/3-a
+         
+                2.2. A continuación, se buscarán links de google drive los cuales contienen las ID's de los videos. Se
+                    Link de drive: https://drive.google.com/file/d/17OKgDQBISNK5ALWE2VX7v8Pc8xT8Cuur/preview
+                    El ID sería:                                   17OKgDQBISNK5ALWE2VX7v8Pc8xT8Cuur
 
-                with open(SAVE_VID_PATH, 'wb') as file: #Guarda el archivo en VIDEO_FILE_INPUT (constants.py)
-                    file.write(download_response.content)
+                2.3. Por último, con esta ID, se encontrarán los links de descarga de google drive de los videos.
+                    Pasa de esto:                https://drive.google.com/file/d/_ID_aquí_/preview
+                    A esto:           https://drive.usercontent.google.com/uc?id=_ID_aquí_/&export=download
+                
+        '''
+        src_s = []
 
-                video_amount += 1
-                print('\nEl archivo ', filename,' ha sido guardado en:', SAVE_VID_PATH)
+        for letra, href in diccionario_href.items():
+            sub_url = url_general + href
+            sub_response = requests.get(sub_url, verify=False)
 
-                if video_amount == 1: #Indica cuantos videos se han descargado via print().
-                    print('\nHasta ahora, se ha descargado 1 archivo.\n')
+            if sub_response.status_code == 200:
+                sub_soup = BeautifulSoup(sub_response.content, 'html.parser')
+                iframes = sub_soup.find_all('iframe', src=True)
+                print()
+
+                if len(iframes) == 0: #Avisar si no encuentra ningún iframe.
+                    print('No iframes encontrados para la letra:',letra,'\n')
                 else:
-                    print('\nHasta ahora, se han descargado',video_amount,'archivos.\n')
-
+                    print(letra)
+                    print()
+                    for iframe in iframes:
+                        src = iframe.get('src')
+                        src_s.append(src)
+                        print(src)
+                    print()
             else:
-                print('No se encontró el nombre del archivo en los encabezados.')
+                print('Error al realizar la solicitud:', sub_response.status_code)
+                break
+                
+        
+        download_links = []
 
+        print('Links de descarga:\n')
+        for src in src_s:
+            raw_download_url = src.replace('https://drive.google.com/file/d/','https://drive.usercontent.google.com/uc?id=')
+            download_url = raw_download_url.replace('/preview', '&export=download')
+            print(download_url)
+            download_links.append(download_url)
 
-    #El diccionario src contiene las letras y los links (google drive) correspondientes.
-    #diccionario_vid_url = dict(zip(letras,video_links))
+        '''
+            ### 3.  De los links de descarga, se extraen los archivos (videos). Los archivos son guardados dentro de
+                    VIDEO_FILE_INPUT_PATH.
+        '''
+        video_amount = 0
 
-    #print("\nDiccionario listo (letras y video links): ")
-    #for letra, video in diccionario_vid_url.items():
-    #    print(f"Letra: {letra}")
-    #    print(f"Video link: {video}")
-    #    print()
+        for link in download_links:
+            download_link_response = requests.get(link, verify=False)
+            
+            if download_link_response.status_code == 200:
+                content_disposition = download_link_response.headers.get('content-disposition')
+                
+                #Encuentra el nombre del archivo en los encabezados
+                if content_disposition:
+                    filename = content_disposition.split('filename=')[1].strip('"')
+                    SAVE_VID_PATH = os.path.join(VIDEO_FILE_INPUT_PATH, filename)
 
+                    with open(SAVE_VID_PATH, 'wb') as file: #Guarda el archivo en VIDEO_FILE_INPUT (constants.py)
+                        file.write(download_link_response.content)
 
-else:
-    print('Error al realizar la solicitud:', response.status_code)
+                    video_amount += 1
+                    print('\nEl archivo ', filename,' ha sido guardado en:', SAVE_VID_PATH)
 
-    #TODO:
-    #Descargar los archivos
+                    if video_amount == 1: #Indica cuantos videos se han descargado via print().
+                        print('\nHasta ahora, se ha descargado 1 archivo.\n')
+                    else:
+                        print('\nHasta ahora, se han descargado',video_amount,'archivos.\n')
+                else:
+                    print('No se encontró el nombre del archivo en los encabezados.')
+    else:
+        print('Error al realizar la solicitud:', response.status_code)
+
+if __name__ == "__main__": 
+    webscrap_samples()
